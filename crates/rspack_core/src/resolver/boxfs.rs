de05f55c9fc8753b8e5cd2d5d02a71dd8355ec9a
nilptr <nilptr.js@gmail.com>
2024-12-19 21:18:32 +0800

    chore: save

--- crates/rspack_core/src/resolver/boxfs.rs
+++ crates/rspack_core/src/resolver/boxfs.rs
@@ -1,5 +1,6 @@
 use std::{io, sync::Arc};
 
+use futures::future::BoxFuture;
 use rspack_fs::{Error, FileSystem};
 use rspack_paths::AssertUtf8;
 use rspack_resolver::{FileMetadata, FileSystem as ResolverFileSystem};
@@ -13,39 +14,65 @@ impl BoxFS {
   }
 }
 impl ResolverFileSystem for BoxFS {
-  fn read_to_string(&self, path: &std::path::Path) -> std::io::Result<String> {
-    match self.0.read(path.assert_utf8()) {
-      Ok(x) => String::from_utf8(x).map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err)),
-      Err(Error::Io(e)) => Err(e),
-    }
+  fn read_to_string<'a>(
+    &'a self,
+    path: &'a std::path::Path,
+  ) -> BoxFuture<'a, std::io::Result<String>> {
+    let fut = async move {
+      match self.0.read(path.assert_utf8()) {
+        Ok(x) => {
+          String::from_utf8(x).map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err))
+        }
+        Err(Error::Io(e)) => Err(e),
+      }
+    };
+    Box::pin(fut)
   }
 
-  fn metadata(&self, path: &std::path::Path) -> std::io::Result<FileMetadata> {
-    match self.0.metadata(path.assert_utf8()) {
-      Ok(meta) => Ok(FileMetadata {
-        is_dir: meta.is_directory,
-        is_file: meta.is_file,
-        is_symlink: meta.is_symlink,
-      }),
-      Err(Error::Io(e)) => Err(e),
-    }
+  fn metadata<'a>(
+    &'a self,
+    path: &'a std::path::Path,
+  ) -> BoxFuture<'a, std::io::Result<FileMetadata>> {
+    let fut = async move {
+      match self.0.metadata(path.assert_utf8()) {
+        Ok(meta) => Ok(FileMetadata {
+          is_dir: meta.is_directory,
+          is_file: meta.is_file,
+          is_symlink: meta.is_symlink,
+        }),
+        Err(Error::Io(e)) => Err(e),
+      }
+    };
+    Box::pin(fut)
   }
 
-  fn symlink_metadata(&self, path: &std::path::Path) -> std::io::Result<FileMetadata> {
-    match self.0.symlink_metadata(path.assert_utf8()) {
-      Ok(meta) => Ok(FileMetadata {
-        is_dir: meta.is_directory,
-        is_file: meta.is_file,
-        is_symlink: meta.is_symlink,
-      }),
-      Err(Error::Io(e)) => Err(e),
-    }
+  fn symlink_metadata<'a>(
+    &'a self,
+    path: &'a std::path::Path,
+  ) -> BoxFuture<'a, std::io::Result<FileMetadata>> {
+    let fut = async move {
+      match self.0.symlink_metadata(path.assert_utf8()) {
+        Ok(meta) => Ok(FileMetadata {
+          is_dir: meta.is_directory,
+          is_file: meta.is_file,
+          is_symlink: meta.is_symlink,
+        }),
+        Err(Error::Io(e)) => Err(e),
+      }
+    };
+    Box::pin(fut)
   }
 
-  fn canonicalize(&self, path: &std::path::Path) -> std::io::Result<std::path::PathBuf> {
-    match self.0.canonicalize(path.assert_utf8()) {
-      Ok(path) => Ok(path.into()),
-      Err(Error::Io(e)) => Err(e),
-    }
+  fn canonicalize<'a>(
+    &'a self,
+    path: &'a std::path::Path,
+  ) -> BoxFuture<'a, std::io::Result<std::path::PathBuf>> {
+    let fut = async move {
+      match self.0.canonicalize(path.assert_utf8()) {
+        Ok(path) => Ok(path.into()),
+        Err(Error::Io(e)) => Err(e),
+      }
+    };
+    Box::pin(fut)
   }
 }
